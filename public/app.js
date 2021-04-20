71c80d309d0643ef6f445949a046ce628ff3ef06
thuan <phamtanthuan2311@gmail.com>
2021-04-20 23:53:05 +0700

    fix(app.js): merge master

--- public/app.js
+++ public/app.js
@@ -9,35 +9,19 @@ const socket = io();
       };
     },
     template: `
-<<<<<<< HEAD
-    <header>
-    </header>
-    <body>
-    <div class="message-container" v-for="message in rederingMessages">
-=======
     <div class="message-container" v-for="message in messages">
->>>>>>> master
       <div class="author-info"><img :src="message.avatarUrl"/> {{message.authorName}} {{message.authorRole}}</div>
       <p class="message-text"><b>{{message.text}}</b></p>
-      <img src="./img/icon_archive.svg" alt="archive"/>
-      <button></button>
-    </div>
-    </body>
-      `,
+    </div>`,
     async mounted() {
 //       author: "Tiến Nguyễn"
 // avatarUrl: "https://yt3.ggpht.com/ytc/AAUvwnhPBnzVVk2vgEQ8ZcJaLe2Wf-rhe8pLAkkTSqwqWg=s88-c-k-c0x00ffffff-no-rj"
 // id: "LCC.CikqJwoYVUNBMnRGazJOZHpPektwU0pkZjI0Y0FnEgtCc3kwV0RPTE5PZxI6ChpDSkx3OWViS2hmQUNGZUFOclFZZER6NEpVdxIcQ042MGhmTEFoZkFDRlU0QXR3QWRyWGdOLUExNQ"
 // publishedAt: "2021-04-17T15:23:14.842695+00:00"
 // text: "oke"
-<<<<<<< HEAD
-
-      await this.fetchMessages();
-=======
->>>>>>> master
       await this.fetchAuthors();
       await this.fetchMessages();
-      
+
       socket.on("New author", this.newAuthorEventHandler);
       socket.on("New message", this.newMessageEventHandler) ;
     },
